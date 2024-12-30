12b0ae77e3c870b2bb825870f02a90d8c8a4cf14
Alvin strandberg <Str4ndb3rg@live.se>
2024-12-30 01:08:53 +0100

    Add project files.

---
+++ src/ChallengeStarter/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ChallengeStarter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
